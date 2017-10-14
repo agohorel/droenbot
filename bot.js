3cb8a8ca74b96bad1e1dda2f2d51cead30c2c94a
agohorel <alex.oddlogic@gmail.com>
2017-10-14 02:16:48 -0400

    remove commando (for now)

--- bot.js
+++ bot.js
@@ -1,8 +1,18 @@
-const commando = require("discord.js-commando");
-const bot = new commando.Client();
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-bot.registry.registerGroup("random", "Random");
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
+bot.on("ready", () =>{
+	console.log("droenbot is live!");
+});
+
+bot.on("message", (message) =>{
+	if(message.content.startsWith("ping")){
+		message.channel.send("pong");
+	} else {
+		if(message.content.startsWith("derp")){
+			message.channel.send("https://imgur.com/a/UV6L7");
+		}
+	}
+});
 
 bot.login("MzY3ODk0MDUyMjQ4NzQ4MDMy.DMCDlw.5Ix5YX5ZQwK1QKBwBvmXKuryyUg");
