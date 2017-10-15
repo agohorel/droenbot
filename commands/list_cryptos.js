f359b3e5481e789c19043e050e6dce541c2bba9d
agohorel <alex.oddlogic@gmail.com>
2017-10-15 19:50:11 -0400

    add !list_cryptos
    lists assets on crytowat.ch

---
+++ commands/list_cryptos.js
@@ -0,0 +1,15 @@
+const request = require("request");
+
+exports.run = (bot, message, args) => {
+	request("https://api.cryptowat.ch/assets", function(error, response, body){
+	   if (!error && response.statusCode == 200){
+	       var data = JSON.parse(body);
+	       var string;
+	       data["result"].forEach(function(asset){
+	       		string += asset["name"] + ": " + asset["id"].toUpperCase() + "\n";
+	       });
+	       message.channel.send(string);
+	   } 
+	});
+}
+
