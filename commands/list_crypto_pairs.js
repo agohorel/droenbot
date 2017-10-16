d569f7f7d185e5b7e477c1a4ae0cf3588ed1286c
agohorel <alex.oddlogic@gmail.com>
2017-10-15 20:28:35 -0400

    search for crypto pairs by exchange
    added an argument to the !list_crypto_pairs command to filter by
    exchange

--- commands/list_crypto_pairs.js
+++ commands/list_crypto_pairs.js
@@ -1,15 +1,17 @@
 const request = require("request");
 
 exports.run = (bot, message, args) => {
-	request("https://api.cryptowat.ch/pairs", function(error, response, body){
+	let [exchange] = args;
+	request("https://api.cryptowat.ch/markets", function(error, response, body){
 	   if (!error && response.statusCode == 200){
 	       var data = JSON.parse(body);
 	       var string;
-	       data["result"].forEach(function(pair){
-	       		string += pair["id"] + "\n";
+	       data["result"].forEach(function(market){
+	       		if (market["active"] == true && `${exchange}` == market["exchange"]){
+	       			string += market["exchange"] + ": " + market["pair"].toUpperCase() + "\n";
+	       		} 
 	       });
 	       message.channel.send(string);
 	   } 
 	});
 }
-
