defd5ec081eee4946070f65f947bef67b4911181
agohorel <alex.oddlogic@gmail.com>
2017-10-20 21:30:36 -0400

    add diff to roll comamnd

--- commands/roll.js
+++ commands/roll.js
@@ -1,16 +1,26 @@
 const config = require("../config.json");
 
 exports.run = (bot, message, args) => {
-	let [iterations, faces] = args;
+	let [iterations, faces, diff] = args;
 	var diceroll, rolls;
 	var total = 0;
+	diff = Math.ceil(1 * `${iterations}` * (`${faces}` * `${diff}`));
 
 	for (var i = 0; i < `${iterations}`; i++){
 		diceroll = Math.floor(Math.random() * `${faces}`) + 1;
 		rolls += diceroll + "\n";
 		total += diceroll;
 	}
+	
+	var result = total - diff;
+
 	rolls = rolls.replace("undefined", "");
-	console.log(typeof total);
-	message.channel.send("You rolled: \n" + rolls + "\n" + "**For a total score of: **" + total);
+
+	if (total >= diff){
+		message.reply(`u **won** with a result of **${result}** (score: ${total}, diff: ${diff}`);
+	} else {
+		message.reply(`u **lost** with a result of **${result}** (score: ${total}, diff: ${diff}`);
+	}
+	
+	//message.channel.send("You rolled: \n" + rolls + "\n" + "**For a total score of: **" + total);
 }
