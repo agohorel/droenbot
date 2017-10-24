3a2c1d7452df011a4e28f2840940d16e9df6c741
agohorel <alex.oddlogic@gmail.com>
2017-10-23 20:33:06 -0400

    encode query for url

--- commands/moviesearch.js
+++ commands/moviesearch.js
@@ -1,13 +1,14 @@
 const request = require("request");
 
 exports.run = (bot, message, args) => {
-	let [query] = args;
- var apiKey = "&apikey=thewdb"; 
+	let query = encodeURI(args);
+ 	var apiKey = "&apikey=thewdb"; 
     var url = "http://www.omdbapi.com/?t="; 
-    request(url + `${query}` + apiKey, function(error, response, body){
+    request(url + query + apiKey, function(error, response, body){
        if (!error && response.statusCode == 200){
            var data = JSON.parse(body);
            var string;
+           console.log(query);
            Object.keys(data).forEach(key => {
            		string += "**"+ key + "**" + ": " + data[key] + "\n";
            });
