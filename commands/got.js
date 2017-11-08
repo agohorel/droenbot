7a5a5c6d8833c98345e31f35a7c28fb8cd0eb177
agohorel <alex.oddlogic@gmail.com>
2017-11-08 00:18:56 -0500

    exception handling for dead !got searches

--- commands/got.js
+++ commands/got.js
@@ -7,37 +7,42 @@ exports.run = (bot, message, args) => {
 	
 	if (type == "char"){
 		var url = "https://anapioficeandfire.com/api/characters/?name=" + `${name}`;
+	
 		request(url, function(error, response, body){
 			if (!error && response.statusCode == 200){
-				var data = JSON.parse(body);
-				data = data[0];
+				try{
+					var data = JSON.parse(body);
+					data = data[0];
 
-				data.aliases.forEach(function(alias){
-					aliases += alias + ", ";
-				});
-				aliases = aliases.replace("undefined", "");
-				aliases = aliases.slice(0, aliases.length - 2);
+					data.aliases.forEach(function(alias){
+						aliases += alias + ", ";
+					});
 
-				message.channel.send({embed: {
-					color: 3447003,
-					fields: [
-					{
-						name: data.name + ", " + data.titles[0],
-						value: "Born " + data.born
-					},
-					{
-						name: "Also known as:",
-						value: aliases 
-					},
-					{
-						name: "Played by:",
-						value: data.playedBy[0]
-					}]
-				} 
-				});
-			}	else {
-					console.log(error);
-					message.channel.send("Huh. Something went wrong.");
+					aliases = aliases.replace("undefined", "");
+					aliases = aliases.slice(0, aliases.length - 2);
+
+					message.channel.send({embed: {
+						color: 3447003,
+						fields: [
+						{
+							name: data.name + ", " + data.titles[0],
+							value: "Born " + data.born
+						},
+						{
+							name: "Also known as:",
+							value: aliases 
+						},
+						{
+							name: "Played by:",
+							value: data.playedBy[0]
+						}]
+					}});
+				}
+				
+				catch(error){
+					message.channel.send(error.name + ": " + error.message);
+					return;
+				}
 			}
 		});
 	}
