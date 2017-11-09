9c979546e3b9d7042a7f1df93a16c006be8564a8
agohorel <alex.oddlogic@gmail.com>
2017-11-08 23:56:04 -0500

    add houses param to !got

--- commands/got.js
+++ commands/got.js
@@ -46,6 +46,42 @@ exports.run = (bot, message, args) => {
 			}
 		});
 	}
+
+	if (type == "house"){
+		var url = "https://anapioficeandfire.com/api/houses/?name=" + `${name}`; 
+
+		request(url, function(error, response, body){
+			if(!error && response.statusCode == 200){
+				
+				try{
+					var data = JSON.parse(body);
+					data = data[0];
+
+					message.channel.send({embed: {
+						color: 3447003,
+						fields: [
+						{
+							name: data.name,
+							value: data.words
+						},
+						{
+							name: "Founded in: ",
+							value: data.region + " in the " + data.founded 
+						},
+						{
+							name: "Seats:",
+							value: data.seats[0]
+						},
+						{
+							name: "Coat of arms:",
+							value: data.coatOfArms
+						}]
+					}});
+				}
+
+				catch(error){
+					message.channel.send(error.name + ": " + error.message);
+					return;
 				}
 			}
 		});
