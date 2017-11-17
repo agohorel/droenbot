2cdf9faa1ca100b324868076f384ada9db8798a9
agohorel <alex.oddlogic@gmail.com>
2017-11-16 21:02:52 -0500

    improvements for !ri

--- commands/ri.js
+++ commands/ri.js
@@ -1,16 +1,31 @@
 exports.run = (bot, message, args) => {
-	let input = args.slice(0).join(" ");
+	let input = args.slice(0).join(" ").toLowerCase();
 	var char, string;
+	
+	console.log(input);
+	input = input.replace(/[^\w\s]/gi, "");
+	console.log(input);
 
 	for (var i = 0; i < input.length; i++){
 		char = input[i];
 		if (char === " "){
-			string += " ";
-		} else{
+			string += "   ";
+		} else if (char === "b" || char === "B") {
+			string += ":b:";
+		} else {
 			string += ":regional_indicator_" + char + ":";
 		}		
 	}
+	
 	message.delete();
-	string = string.replace("undefined", "");
-	message.channel.send(string);
+	
+	try{
+		string = string.replace("undefined", "");
+		message.channel.send(string);
+	}
+
+	catch(error){
+		console.log(error);
+		return;
+	}
 }
