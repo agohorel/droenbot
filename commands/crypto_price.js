9f11d206e74a387d68a39d76d6e5338146c94b9c
agohorel <alex.oddlogic@gmail.com>
2017-11-25 17:45:44 -0500

    improved crypto_price
    now includes 24 hr. high, low, % change, and current price placed in an
    embed. the color of the embed changes based on the % change.

--- commands/crypto_price.js
+++ commands/crypto_price.js
@@ -1,11 +1,50 @@
 const request = require("request");
+var col;
 
 exports.run = (bot, message, args) => {
 	let [market, pair] = args;
-	request(`https://api.cryptowat.ch/markets/${market}/${pair}/price`, function(error, response, body){
+	request(`https://api.cryptowat.ch/markets/${market}/${pair}/summary`, function(error, response, body){
 		if (!error && response.statusCode == 200){
 			var data = JSON.parse(body);
-			message.channel.send(data["result"].price.toFixed(8));
+			var last = data["result"].price.last;
+			var high = data["result"].price.high;
+			var low = data["result"].price.low;
+			var change_raw = data["result"].price.change.percentage;
+			var change_formatted = (change_raw * 100).toFixed(2) + "%";
+			
+			
+			if (change_raw > 0){
+				col = 3394611;
+			} else {
+				col = 13382451;
+			}
+
+			message.channel.send({embed: {
+				title: `Price for ${pair} on ${market}:`.toUpperCase(),
+			    color: col,
+			    fields: [{
+			        name: "LAST",
+			        value: last.toString()
+			      },
+			      {
+			      	name: "% CHANGE",
+			      	value: change_formatted
+			      },
+			      {
+			        name: "24 HR. HIGH",
+			        value: high.toString()
+			      },
+			      {
+			        name: "24 HR. LOW",
+			        value: low.toString()
+			      }
+			    ],
+			    footer: {
+			      text: "data sourced from cryptowat.ch"
+			    }
+			  }
+			});
+
 		} else{
 			message.channel.send("Huh, something went wrong. The correct syntax is `!crypto_price exchange trading pair`");
 		}
