7f0567948825c1a78265d7ec94df1deab3112792
agohorel <alex.oddlogic@gmail.com>
2017-12-09 14:49:19 -0500

    convert numbers to emojis

--- commands/memetext.js
+++ commands/memetext.js
@@ -11,11 +11,31 @@ exports.run = (bot, message, args) => {
 			string += "   ";
 		} else if (char === "b" || char === "B") {
 			string += ":b:";
+		} else if (char === "1") {
+			string += ":one:";
+		} else if (char === "2") {
+			string += ":two:";
+		} else if (char === "3") {
+			string += ":three:";
+		} else if (char === "4") {
+			string += ":four:";
+		} else if (char === "5") {
+			string += ":five:";
+		} else if (char === "6") {
+			string += ":six:";
+		} else if (char === "7") {
+			string += ":seven:";
+		} else if (char === "8") {
+			string += ":eight:";
+		} else if (char === "9") {
+			string += ":nine:";
+		} else if (char === "0") {
+			string += ":zero:";
 		} else {
 			string += ":regional_indicator_" + char + ":";
 		}		
 	}
-	
+
 	message.delete();
 	
 	try{
