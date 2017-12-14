21b6d787f5f9adf3d86f08234d91a7f2e6676b27
agohorel <alex.oddlogic@gmail.com>
2017-12-14 01:08:56 -0500

    rich embedified weather

--- commands/weather.js
+++ commands/weather.js
@@ -1,20 +1,29 @@
+const Discord = require("discord.js");
 const request = require("request");
 
 exports.run = (bot, message, args) => {
 	let [city, state] = args;
-	var string;
+	var string = "";
+	var color = [16, 123, 181];
 	request(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${city}%2C%20${state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`, function(error, response, body){
 		if (!error && response.statusCode == 200){
 			var data = JSON.parse(body);
-			string += "*" + "Weather for " + data.query.results.channel.location.city + "," + data.query.results.channel.location.region 
-			+ " (" + data.query.results.channel.location.country + ")" + "*" + "\n" + "\n";
-
+			var city = data.query.results.channel.location.city;
+			var region = data.query.results.channel.location.region;
+			var country = data.query.results.channel.location.country;
+		
 			data.query.results.channel.item.forecast.forEach(function(forecast){
 				string += "**" + forecast.day + "**" + ": " + forecast.text + ". " + "High: " + forecast.high + ", " + "Low: " + forecast.low + ", " + "\n";
 			});
-			string = string.replace("undefined", "");
-			message.channel.send(string);
-		} else{
+
+			var embed = new Discord.RichEmbed()
+				.setTitle(`Weather for ${city}, ${region} (${country})`)
+				.setDescription(string)
+				.setColor(color);
+
+			message.channel.send(embed);
+		
+		} else {
 			message.channel.send("huh, something went wrong. try again?");
 		}
 	});
