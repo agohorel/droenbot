2eac622061eba5d2e4b1f35bad3edc789d0a0731
agohorel <alex.oddlogic@gmail.com>
2017-12-31 18:54:52 -0500

    restrict !reload to mod role

--- commands/reload.js
+++ commands/reload.js
@@ -1,5 +1,11 @@
 exports.run = (client, message, args) => {
-  if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
-  delete require.cache[require.resolve(`./${args[0]}.js`)];
-  message.reply(`The command ${args[0]} has been reloaded`);
+	const mod = message.guild.roles.find("name", "testmod");
+
+ 	if (!message.member.roles.has(mod.id)) {
+    	return message.reply("You do not have sufficient permissions to use this command.");
+	}		
+
+    if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
+    delete require.cache[require.resolve(`./${args[0]}.js`)];
+    message.reply(`The command ${args[0]} has been reloaded`);
 };
