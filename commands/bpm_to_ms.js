1c79129254ed6f073ad8f888cd679c8b3ef6ad52
agohorel <alex.oddlogic@gmail.com>
2018-01-03 21:30:15 -0500

    fix !bpm_to_ms
    d'oh!

--- commands/bpm_to_ms.js
+++ commands/bpm_to_ms.js
@@ -2,7 +2,7 @@ var result, string;
 
 exports.run = (bot, message, args) => {
 	let [division, bpm] = args;
-	result = (60000 / parseFloat(bpm)) * eval(division);
+	result = (60000 / parseFloat(bpm)) * (eval(division) * 4);
 	string = "A " + division + " note at " + bpm + " BPM equals **" + result + "ms**";
 	message.channel.send(string);
 }
