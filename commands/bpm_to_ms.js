e8f2e23af566d3b0eb7a6c645a4ad8d5437597b1
agohorel <alex.oddlogic@gmail.com>
2018-01-07 10:00:55 -0500

    improved !bpm_to_ms
    list/help option, prompts user when input isn't supported, and
    embeddified output

--- commands/bpm_to_ms.js
+++ commands/bpm_to_ms.js
@@ -1,8 +1,46 @@
-var result, string;
+var result,
+    string = "",
+  	list = "";
+
+let subdivisions = {
+    "1/2": .5,
+    "1/3": .33334,
+    "1/4": .25,
+    "1/6": .16667,
+    "1/8": .125,
+    "1/12": .08333,
+    "1/16": .0625,
+    "1/24": .04167,
+    "1/32": .03125
+}
 
 exports.run = (bot, message, args) => {
-	let [division, bpm] = args;
-	result = (60000 / parseFloat(bpm)) * (eval(division) * 4);
-	string = "A " + division + " note at " + bpm + " BPM equals **" + result + "ms**";
-	message.channel.send(string);
+    let [division, bpm] = args;
+
+    if (division === "list" || division === "help" || division === undefined ){
+        Object.keys(subdivisions)
+            .forEach(function(subdivision){
+                list += subdivision + "\n";
+            });
+        message.channel.send({embed: {
+            title: "List of supported subdivisions:",
+            description: list 
+        }});
+        list = "";
+        return;
+    }
+
+    else if (subdivisions[division] === undefined){
+        message.reply("You must specify a supported subdivision. To see a list of supported subdivisions, use `!bpm_to_ms list`.");
+        return;
+    }
+
+    else {
+        result = (60000 / parseFloat(bpm)) * (subdivisions[division] * 4);
+        string = "A " + division + " note at " + bpm + " BPM equals:";
+        message.channel.send({embed: {
+            title: string,
+            description: `${result}ms` 
+        }});
+    }
 }
