5bcdf434f26324a739d9bf0f08fe6b937e5e1ca6
agohorel <alex.oddlogic@gmail.com>
2018-02-12 23:57:21 -0500

    insult tenses & duplicate checking
    starting to break things into functions that can accept tenses and check
    for duplicates to hopefully allow for more procedural approaches to
    constructing sentences. maybe. i will have to learn basic grammar first.

--- commands/insult.js
+++ commands/insult.js
@@ -1,13 +1,60 @@
-let curseObjects = ["fuck", "cunt", "weeb", "dildo", "moron", "idiot", "simpleton", "dingus", "fuccboi", "squirt", "shrimp", "taint", "gooch"],
-	curseSubjects = ["shit", "piss", "jizz", "fart", "vomit", "shart", "queef", "ass", "dick", "cock", "taint", "gooch", "perineum", "nipple"],
-	curseVerbs = ["eating", "drinking", "slurping", "snorting", "chugging", "blasting", "foraging", "licking", "fondling", "scrubbing"];
+let objects = ["fuck", "cunt", "weeb", "dildo", "moron", "simpleton", "dingus", "fuccboi", "squirt", "shrimp", "taint", "gooch"],
+	subjects = ["shit", "piss", "jizz", "fart", "vomit", "queef", "ass", "dick", "cock", "taint", "gooch", "nipple"],
+	verbs = ["eat", "drink", "slurp", "snort", "blast", "lick", "suck", "fuck", "kick", "roll"],
+	adjectives = ["stupid", "idiotic", "thoughtless", "dull", "boring", "abnoxious", "weak", "ineffectual", "impotent", "unsophisticated", "uncultured", "uneducated", "unskilled", "ignorant", "inconsequential"],
+	lastAdjective = "", lastObject = "", lastVerb = "", lastSubject = "";
+
 
 exports.run = (client, message, args) => {
-	let member = message.mentions.members.first(),
-		selectedObject = curseObjects[Math.floor(Math.random() * curseObjects.length)],
-		selectedSubject = curseSubjects[Math.floor(Math.random() * curseSubjects.length)],
-		selectedVerb = curseVerbs[Math.floor(Math.random() * curseVerbs.length)];
+	let member = message.mentions.members.first();
 
 	message.delete();
-	message.channel.send(member + `, you're a ${selectedSubject} ${selectedVerb} ${selectedObject}.`);
+
+	var date = new Date();
+	var seconds = date.getSeconds();
+
+	if (seconds % 2 === 0){
+		message.channel.send(member + ", you're a " + makeSubject() + " " + makeVerb("ing") + " " + makeObject() + ".");
+	} else {
+		message.channel.send("You know " + member + ", it's remarkable that someone as " + makeAdjective() + " and " + makeAdjective() + " as you can still manage to fuck up " + makeVerb("ing") + " a " + makeObject() + ".");
+	}
 };
+
+function makeVerb(tense) {
+	var verb = verbs[Math.floor(Math.random() * verbs.length)] + tense;	
+	// check for duplicates
+	if (verb === lastVerb){
+		verb = verbs[Math.floor(Math.random() * verbs.length)] + tense;;
+	}
+	lastVerb = verb;
+	return verb; 
+}
+
+function makeObject() {
+	var object = objects[Math.floor(Math.random() * objects.length)];
+	if (object === lastObject){
+		object = objects[Math.floor(Math.random() * objects.length)];
+	}
+	lastObject = object;
+	return object;
+}
+
+function makeSubject() {
+	var subject = subjects[Math.floor(Math.random() * subjects.length)];
+	
+	if (subject === lastSubject){
+		subject = subjects[Math.floor(Math.random() * subjects.length)];
+	}
+	lastSubject = subject;
+
+	return subject;
+}
+
+function makeAdjective() {
+	var adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+	if (adjective === lastAdjective){
+		adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+	}
+	lastAdjective = adjective;
+	return adjective;
+}
