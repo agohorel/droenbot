e0332a048b1b7c45863cc5167a886e42ed935ceb
agohorel <alex.oddlogic@gmail.com>
2019-02-24 17:29:36 -0500

    update write to test against writeToDirectory

--- commands/write.js
+++ commands/write.js
@@ -4,13 +4,33 @@ const path = require("path");
 
 exports.run = (bot, message, args) => {
 	let myPath = path.resolve(__dirname, "../" ,"data/directory");
-	console.log(__dirname);
-	console.log(myPath);
+	let imgLink = null;
+	let timezone = null;
+	let userData = {};
 
-	let userData = {
-		name: "whoever",
-		about: "whatever"
-	};
+	for (let i = 0; i < args.length; i++){
+		if (args[i].indexOf("timezone:") > -1){
+			// trim "timezone:" tag off
+			timezone = args[i].substring(9, args[i].length);
+			// remove element from array of non-specific links
+			args.splice(i, 1);
+		}
+		
+		if (args[i].indexOf("img:") > -1){
+			imgLink = args[i].substring(4, args[i].length);
+			args.splice(i, 1);
+		}
+	}
+
+	// loop through newly truncated args array
+	for (let i = 0; i < args.length; i++){
+		// build up object
+		userData[i] = args[i];
+	}
+
+	// attach previously spliced out array elements individually
+	userData.img = imgLink;
+	userData.timezone = timezone;
 
 	fs.writeFile(path.join(myPath, `${message.member.displayName}.json`), JSON.stringify(userData, null, 2), (err) => {
 		err ? console.log(err) : console.log("wrote file");
