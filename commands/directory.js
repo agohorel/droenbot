2a36da35f6571d4afe0911ad9f0fad6233503482
agohorel <alex.oddlogic@gmail.com>
2019-02-24 19:14:58 -0500

    list directory entries if request fails

--- commands/directory.js
+++ commands/directory.js
@@ -1,24 +1,44 @@
 const Discord = require("discord.js");
+const fs = require("fs");
 const path = require("path");
 
 exports.run = (bot, message, args) => {
 	let username = args.slice(0).join(" ").trim().toLowerCase();
-	let myPath = path.resolve(__dirname, "../" ,"data/directory");
-	let userData = require(path.join(myPath, `/${username}.json`));
-	let string = "";
+	let myPath = path.resolve(__dirname, "../" ,"data/directory");	
+	
+	// if requested user can be found, display their directory info
+	try {
+		let userData = require(path.join(myPath, `/${username}.json`));
+		let userLinks = "";
+		// display the requested user
+		Object.keys(userData).forEach((key) => {
+			if (key !== "img" && key !== "timezone"){
+				userLinks += `${userData[key]}\n`;
+			}
+		});
 
-	Object.keys(userData).forEach((key) => {
-		if (key !== "img" && key !== "timezone"){
-			string += `${userData[key]}\n`;
-		}
-	});
+		let userEmbed = new Discord.RichEmbed()
+			.setTitle(username)
+			.setImage(userData.img)
+			.addField("links: ", userLinks)
+			.addField("timezone: ", userData.timezone)
+			.setColor([75, 75, 75]);
 
-	let embed = new Discord.RichEmbed()
-		.setTitle(username)
-		.setImage(userData.img)
-		.addField("links: ", string)
-		.addField("timezone: ", userData.timezone)
-		.setColor([75, 75, 75]);
+		message.channel.send(userEmbed);
+	}
+	// if requested user cannot be found, display the directory list
+	catch(e){
+		let userList = "";
+		// read "data/directory" directory
+		fs.readdirSync(myPath).forEach((user) => {
+			userList += `${user.substring(0, user.length-5)}\n`;
+		});
 
-	message.channel.send(embed);
+		let userListEmbed = new Discord.RichEmbed()
+			.setTitle("list of users in directory")
+			.setDescription(userList)
+			.setColor([150, 150, 150]);
+
+		message.channel.send(userListEmbed);
+	}
 }
