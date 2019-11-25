9415f1baa8ed1f2533c21c47d20e00af897476f7
agohorel <gohorelalex@gmail.com>
2019-11-25 14:01:10 -0500

    add getVotes helper function

--- commands/vote.js
+++ commands/vote.js
@@ -4,7 +4,6 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   const description = args.slice(0, args.length - 1).join(" ");
   const duration = args[args.length - 1] * 60000; // express duration as minutes
-  let yeas, nays;
 
   const poll = new Discord.RichEmbed()
     .setTitle("Poll")
@@ -17,17 +16,8 @@ exports.run = async (bot, message, args) => {
 
   let results = await pollMessage.awaitReactions(filter, { time: duration });
 
-  try {
-    yeas = await results.get("✅").count;
-  } catch (error) {
-    yeas = 0;
-  }
-
-  try {
-    nays = await results.get("❎").count;
-  } catch (error) {
-    nays = 0;
-  }
+  let yeas = getVotes("✅");
+  let nays = getVotes("❎");
 
   const resultsEmbed = new Discord.RichEmbed()
     .setTitle(
@@ -47,3 +37,11 @@ exports.run = async (bot, message, args) => {
 
   message.channel.send(resultsEmbed);
 };
+
+function getVotes(emoji){
+  try {
+    return await results.get(emoji).count;
+  } catch (error) {
+    return 0;
+  }
+}
