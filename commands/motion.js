a13f03d80a51730ad4e79be130ef3e32d6dec99b
agohorel <gohorelalex@gmail.com>
2019-11-26 16:17:47 -0500

    if no duration given, return syntax helper for !motion

--- commands/motion.js
+++ commands/motion.js
@@ -5,39 +5,45 @@ exports.run = async (bot, message, args) => {
   const description = args.slice(0, args.length - 1).join(" ");
   const duration = args[args.length - 1] * 60000; // express duration as minutes
 
-  const poll = new Discord.RichEmbed()
-    .setTitle("Motion")
-    .setDescription(description);
-
-  const pollMessage = await message.channel.send(poll);
-
-  const filter = reaction =>
-    reaction.emoji.name === "✅" || reaction.emoji.name === "❎";
-
-  let results = await pollMessage.awaitReactions(filter, { time: duration });
-
-  let yeas = getVotes("✅", results);
-  let nays = getVotes("❎", results);
-
-  const resultsEmbed = new Discord.RichEmbed()
-    .setTitle(
-      `Results for *Motion to ${description}*, created by ${message.member.displayName}`
-    )
-    .addField("**yeas:**", `${yeas}`)
-    .addField("**nays:**", `${nays}`)
-    .setColor(
-      yeas > nays
-        ? "#32a852"
-        : nays > yeas
-        ? "#a83232"
-        : yeas === nays
-        ? "#d9760d"
-        : "#FFFFFF"
+  if (typeof args[args.length - 1] !== "number") {
+    message.channel.send(
+      "Please provide a duration for the vote in minutes. \nSyntax: `!motion to [do something] [duration in minutes]`"
     );
+  } else {
+    const poll = new Discord.RichEmbed()
+      .setTitle("Motion")
+      .setDescription(description);
 
-  message.channel.send(resultsEmbed);
+    const pollMessage = await message.channel.send(poll);
 
-  crud.writeFile(`${description}.json`, "votes", { yeas, nays });
+    const filter = reaction =>
+      reaction.emoji.name === "✅" || reaction.emoji.name === "❎";
+
+    let results = await pollMessage.awaitReactions(filter, { time: duration });
+
+    let yeas = getVotes("✅", results);
+    let nays = getVotes("❎", results);
+
+    const resultsEmbed = new Discord.RichEmbed()
+      .setTitle(
+        `Results for *Motion to ${description}*, created by ${message.member.displayName}`
+      )
+      .addField("**yeas:**", `${yeas}`)
+      .addField("**nays:**", `${nays}`)
+      .setColor(
+        yeas > nays
+          ? "#32a852"
+          : nays > yeas
+          ? "#a83232"
+          : yeas === nays
+          ? "#d9760d"
+          : "#FFFFFF"
+      );
+
+    message.channel.send(resultsEmbed);
+
+    crud.writeFile(`${description}.json`, "votes", { yeas, nays });
+  }
 };
 
 function getVotes(emoji, results) {
