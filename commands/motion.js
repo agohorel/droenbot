ab3f7b0ddf475eaceacca9701ac126fa4241dbaf
agohorel <alex.oddlogic@gmail.com>
2019-12-13 20:12:53 -0500

    fixes/improvements to motion.js
    provide voting instructions in embed, mention @everyone, and fix typechecking bug for durations

--- commands/motion.js
+++ commands/motion.js
@@ -5,15 +5,20 @@ exports.run = async (bot, message, args) => {
   const description = args.slice(0, args.length - 1).join(" ");
   const duration = args[args.length - 1] * 60000; // express duration as minutes
 
-  if (typeof args[args.length - 1] !== "number") {
+  if (typeof Number(args[args.length - 1]) !== "number") {
     message.channel.send(
       "Please provide a duration for the vote in minutes. \nSyntax: `!motion to [do something] [duration in minutes]`"
     );
   } else {
     const poll = new Discord.RichEmbed()
       .setTitle("Motion")
-      .setDescription(description);
+      .setDescription(description)
+      .addField(
+        "Voting Instructions",
+        "Vote by reacting to this embed with a ✅ (yea) or ❎ (nay)"
+      );
 
+    message.channel.send("@everyone please vote below!");
     const pollMessage = await message.channel.send(poll);
 
     const filter = reaction =>
